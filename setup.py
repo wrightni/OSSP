2eb9ed5e2ace23b5e5f4ec58334effdadba626ab
Nicholas Wright <nicholas.c.wright.th@dartmouth.edu>
2018-08-27 10:37:53 -0400

    Fixed extension name warning in setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
-USE_CYTHON = True
+USE_CYTHON = False
 
 ext = '.pyx' if USE_CYTHON else '.c'
 
-extensions = [Extension("attribute_calculations", ['lib/attribute_calculations'+ext]),
-              Extension("create_clsf_raster", ["lib/create_clsf_raster"+ext])]
+extensions = [Extension("lib.attribute_calculations", ['lib/attribute_calculations'+ext]),
+              Extension("lib.create_clsf_raster", ["lib/create_clsf_raster"+ext])]
 
 if USE_CYTHON:
     from Cython.Build import cythonize
     extensions = cythonize(extensions)
 
 setup(
-    ext_modules = extensions
+    ext_modules = extensions,
 )
