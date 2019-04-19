2eb3244c93d13f1101ed01423d6060059455483f
Nicholas Wright <nicholas.c.wright.th@dartmouth.edu>
2019-04-19 10:49:14 -0400

    Added fix to histogram peak detection.

--- preprocess.py
+++ preprocess.py
@@ -208,6 +208,12 @@ def find_peaks(hist, bin_centers):
 
     # First find all potential peaks in the histogram
     peaks = []
+
+    # Check the lowest histogram bin
+    if hist[0] >= hist[1] and hist[0] >= hist[5]:
+        peaks.append(bin_centers[0])
+
+    # Check the middle bins
     for i in range(1, len(bin_centers) - 1):
         # Acceptable width of peak is +/-5, except in edge cases
         if i < 5:
@@ -224,6 +230,9 @@ def find_peaks(hist, bin_centers):
                 and hist[i] >= hist[i - w_l] and hist[i] >= hist[i + w_u]):
             if hist[i] > min_count:
                 peaks.append(bin_centers[i])
+    # Check the highest histogram bin
+    if hist[-1] >= hist[-2] and hist[-1] >= hist[-6]:
+        peaks.append(bin_centers[-1])
 
     num_peaks = len(peaks)
     distance = 5  # Initial distance threshold
